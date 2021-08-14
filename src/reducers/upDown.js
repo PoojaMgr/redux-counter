706fc64351dcb169e18b238a790d05682ce6166d
Pooja Thapa <30620762+PoojaMgr@users.noreply.github.com>
2021-08-14 10:19:54 +0530

    Update upDown.js

--- src/reducers/upDown.js
+++ src/reducers/upDown.js
@@ -2,7 +2,6 @@
 const initialState=0;
 
 const changeNumberReducer = (state=initialState, action) => {
-  debugger
     switch(action.type){
         case "INCREMENT": return state+1;
         case "DECREMENT": return state-1;
